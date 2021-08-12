82d5d393859c826e070fb5e02ef11053c80fa430
Erik Vavro <me@madhax.io>
2021-08-11 20:30:20 -0400

    :sparkles: Improved consistency of argument names in math module functions.

--- src/math.js
+++ src/math.js
@@ -1,28 +1,24 @@
 /**
  * Recursively calculates the greatest common denominator (GCD) between two values
  *
- * @param {Number} a
- * @param {Number} b
+ * @param {Number} x
+ * @param {Number} y
  * @returns {Number}
  */
-export function gcd (a, b) {
-  if (b == 0) {
-    return a
-  }
-
-  return gcd(b, a % b)
+export function gcd (x, y) {
+  return y === 0 ? x : gcd(y, x % y)
 }
 
 /**
- * Modifies a value so that it is always between the provided min and max
+ * Modifies a value so that it is always between x and y
  *
  * @param {Number} value
- * @param {Number} min
- * @param {Number} max
+ * @param {Number} x
+ * @param {Number} y
  * @returns {Number}
  */
-export function clamp (value, min = 0, max = 1) {
-  return Math.min(max, Math.max(min, value))
+export function clamp (value, x = 0, y = 1) {
+  return Math.min(y, Math.max(x, value))
 }
 
 /**
